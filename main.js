d31bac392fcd53fa0a55f07d09ed60e3e7dec20e
VerbotenLad <sanad.amish@gmail.com>
2021-11-14 10:53:03 +0400

    created a vue instance and mounted is as an id then added the id to the first div in the body. then I displayed the data.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+//this is a Vue instance which powers our app. It must always
+// be passed an Options Object, even if empty.
+const app = Vue.createApp({
+    data: function(){
+        return{
+            product: 'Socks',
+            description: 'Available in Blue and Green'
+        }
+    }
+})
