b102efdd4557c4ad7ae37ad8d9f5ff3014b20789
VerbotenLad <sanad.amish@gmail.com>
2021-11-16 10:47:09 +0400

    ssd

---
+++ components/productDisplay.js
@@ -0,0 +1,90 @@
+myApp.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        },
+        cart: {
+            type: Int8Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `<div class="product-display">
+        <div class="product-container">
+        <div class="product-image">
+          <img v-bind:src="image">
+        </div>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p>{{ sale }}</p>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <product-details :details="details"></product-details>
+          <div 
+            v-for="(variant, index) in variants" 
+            :key="variant.id" 
+            @mouseover="updateVariant(index)" 
+            class="color-circle" 
+            :style="{ backgroundColor: variant.color }">
+          </div>
+          
+          <button 
+            class="button" 
+            :class="{ disabledButton: !inStock }" 
+            :disabled="!inStock" 
+            v-on:click="addToCart">
+          Add to Cart</button>
+        </div>
+      </div>
+    </div>`,
+      data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            selectedVariant: 0,
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            // solution
+            onSale: true
+            // solution
+        }
+    },
+    methods: {
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        // solution
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
+        },
+        shipping(){
+            if (this.premium){
+                return 'Free'
+            }
+            return '$2.99'
+        },
+        // solution
+    },
+      
+})
